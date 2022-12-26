fff69c830120ff6dc1ca6bb599f08cddcb9fb77a
bkmahapatra <bkmahapatra27@gmail.com>
2022-12-26 15:32:50 +0530

    about complete

--- surajrevannawebsite/src/components/About.jsx
+++ surajrevannawebsite/src/components/About.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
+import React from "react";
+import "../styles/About.css";
+import English from "../Englishlang.json";
 
 const About = () => {
   return (
     <section className="aboutPage" id="about">
-       <div className="sectionHeader">
+      <div className="sectionHeader">
         <div className="sectionHeaderTitle">
-            <div className="title_upbar"></div>
-            <h3 className="sectionTitle">
-                
-            </h3>
-            <div className="title_downbar"></div>
+          <div className="title_upbar"></div>
+          <h3 className="sectionTitle">{English.siteAbout.title}</h3>
+          <div className="title_downbar"></div>
         </div>
-        <div className="sectionDesc"></div>
-       </div>
-       <div className="sectionbody">
-        <img src="" alt="" className="aboutImage" />
-       </div>
+        <div className="sectionDesc">{English.siteAbout.desc}</div>
+        <div className="sectionInfo">
+          {English.siteAbout.body.map((item) => {
+            return (
+              <div>
+                <div className="infoTitle">{item.title}</div>
+                <div className="infoDesc">{item.desc}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div></div>
+      <div className="sectionbody">
+        <img
+          className="aboutImage"
+          src={require("../assests/" + English.siteAbout.img)}
+          alt=""
+        />
+      </div>
     </section>
-    )
-}
+  );
+};
 
-export default About
+export default About;
