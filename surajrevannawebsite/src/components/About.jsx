542af8e0ed75deeb4fbec7df99a345a623fe4852
bkmahapatra <bkmahapatra27@gmail.com>
2022-12-26 14:11:19 +0530

    base complete

---
+++ surajrevannawebsite/src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="aboutPage" id="about">
+       <div className="sectionHeader">
+        <div className="sectionHeaderTitle">
+            <div className="title_upbar"></div>
+            <h3 className="sectionTitle">
+                
+            </h3>
+            <div className="title_downbar"></div>
+        </div>
+        <div className="sectionDesc"></div>
+       </div>
+       <div className="sectionbody">
+        <img src="" alt="" className="aboutImage" />
+       </div>
+    </section>
+    )
+}
+
+export default About
